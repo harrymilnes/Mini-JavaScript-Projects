6730df25be5b1081f394f30b5e5f23a21f8e92f5
Harry Milnes <harry@milnes.org>
2017-07-26 23:29:16 +0100

    Added customisation to make it more interesting, courtesy of dat-gui

--- Three.js Lorenz Attractor/js/attractor.js
+++ Three.js Lorenz Attractor/js/attractor.js
@@ -1,16 +1,24 @@
 var lorenz;
-var config = 
-{
-	rho: 28.0,
-	sigma: 10.0,
-	beta: 8.0/3.0,
-	time: 0.01,
-	points: 10000,
-};
-
 window.onload = function()
 {
 	lorenz = new Lorenz();
+	var gui = new dat.GUI();
+
+	var sys_params_folder = gui.addFolder('System Parameters');
+	sys_params_folder.add(lorenz, 'rho', 0.0, 100.0);
+	sys_params_folder.add(lorenz, 'sigma', 0.0, 100.0);
+	sys_params_folder.add(lorenz, 'beta', 0.0, 100.0);
+	sys_params_folder.add(lorenz, 'time', 0.0, 60.0);
+	sys_params_folder.add(lorenz, 'maximumPoints', 1000, 100000);
+
+	var materials_folder = gui.addFolder('Materials');
+	var colour = materials_folder.addColor(lorenz, 'colour');
+
+	colour.onChange(function( colour ) {
+		lorenz.line.material.color = new THREE.Color( colour );
+	});
+
+	gui.add(lorenz, 'render');
 }
 
 function Lorenz() 
@@ -24,16 +32,31 @@ function Lorenz()
 	this.camera.position.set(-430, 151, -300);
 	this.scene.add(this.camera); 
 
-	this.geometry = new THREE.BufferGeometry().addAttribute( 'position', new THREE.BufferAttribute(CreateLorenzArraySolution(), 3 ));
-	this.line = new THREE.Line( this.geometry );
+	this.geometry = new THREE.BufferGeometry();
+	this.geometryPaths = new Float32Array(config.points * 3);
+	this.geometry.addAttribute('position', new THREE.BufferAttribute(this.geometryPaths, 3 ));
+	this.line = new THREE.Line(this.geometry);
 	this.scene.add(this.line);
 
 	this.controls = new THREE.OrbitControls(this.camera, this.webGlRender.domElement);
-	this.controls.minDistance = 250;
-	this.controls.maxDistance = 550;
-	this.currentPointDrawn = 0;
 
+	this.rho = 28.0;
+	this.sigma = 10.0;
+	this.beta = 8.0/3.0;
+	this.time = 0.01;
+	this.maximumPoints = 10000;
+
+	this.colour = "#ffae23";
+
+	this.render();
 	this.animate();
+}
+
+Lorenz.prototype.render = function()
+{
+	this.CreateLorenzArraySolution();
+	this.currentPointDrawn = 0;
+	this.geometry.attributes.position.needsUpdate = true;
 }
 
 Lorenz.prototype.animate = function()
@@ -43,31 +66,29 @@ Lorenz.prototype.animate = function()
 	requestAnimFrame(this.animate.bind(this));
 }
 
-function CreateLorenzArraySolution()
+Lorenz.prototype.CreateLorenzArraySolution = function()
 {
-	let lorenzSolution = new Float32Array(config.points * 3);
-	let lorezIndex = 0;
+	let self = this;
 
+	let lorezIndex = 0;
 	let x = 0.1;
 	let y = 0.1;
 	let z = 0.1;
 
-	for(var i = 0; i < config.points; i++)
+	for(var i = 0; i < self.maximumPoints; i++)
 	{
-		let dx = config.time * (config.sigma * (y - x));
-		let dy = config.time * (x * (config.rho - z) - y);
-		let dz = config.time * (x * y - config.beta * z);
+		let dx = self.time * (self.sigma * (y - x));
+		let dy = self.time * (x * (self.rho - z) - y);
+		let dz = self.time * (x * y - self.beta * z);
 
 		x += dx;
 		y += dy;
 		z += dz;
 
-		lorenzSolution [lorezIndex++] = x;
-		lorenzSolution [lorezIndex++] = y;
-		lorenzSolution [lorezIndex++] = z;
+		this.geometryPaths[lorezIndex++] = x;
+		this.geometryPaths[lorezIndex++] = y;
+		this.geometryPaths[lorezIndex++] = z;
 	}
-
-	return lorenzSolution;
 }
 
 window.requestAnimFrame = (function() {
