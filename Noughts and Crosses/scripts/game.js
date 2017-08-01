007af3c9fd4812adee6be2b6055f493bcd759b09
Harry Milnes <harry@milnes.org>
2017-08-01 22:10:21 +0100

    Renamed Noughts and Crosses files because of typo & made a start on the jQuery game engine.

--- Noughts and Crosses/scripts/game.js
+++ Noughts and Crosses/scripts/game.js
@@ -1,9 +1,14 @@
-$(function() {
+$(function() 
+{
+    let playerModels = {
+        NOUGHT: 0,
+        CROSS: 1
+    };
 
+    let playerModel = playerModels.NOUGHT;
 
-
-
-
-
-
+    $("#game-mat > .row > div").click(function() 
+    {
+        (playerModel === playerModels.NOUGHT) ? $(this).addClass("nought").text("O") : $(this).addClass("cross").text("X");
+    });
 });
