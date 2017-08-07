297945fc8dae9876d5b2e30619ba10a4e31324ad
Harry Milnes <harry@milnes.org>
2017-08-07 11:08:37 +0100

    Quick commit from Macbook, removed rows from the view & added interactivity of placing Noughts in the JavaScript whilst travelling.

--- Noughts and Crosses/scripts/game.js
+++ Noughts and Crosses/scripts/game.js
@@ -7,8 +7,28 @@ $(function()
 
     let playerModel = playerModels.NOUGHT;
 
-    $("#game-mat > .row > div").click(function() 
+    let winningCombinations = [
+        //Horizontal
+        [0, 1, 2],
+        [3, 4, 5],
+        [2, 5, 8],
+        //Verticle
+        [0, 3, 6],
+        [1, 4, 7],
+        [6, 7, 8],
+        //Diaganol
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    
+    let currentBoardCombination = [];
+
+    $("#game-mat > div").click(function() 
     {
-        (playerModel === playerModels.NOUGHT) ? $(this).addClass("nought").text("O") : $(this).addClass("cross").text("X");
+        if($.inArray($(this).index(), currentBoardCombination) === -1)
+        {
+            (playerModel === playerModels.NOUGHT) ? $(this).addClass("nought").text("O") : $(this).addClass("cross").text("X");
+            currentBoardCombination.push($(this).index());
+        }
     });
 });
