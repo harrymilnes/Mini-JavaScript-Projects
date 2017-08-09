2d15b60d207edf78e7224e9b4f60024a074728af
Harry Milnes <harry@milnes.org>
2017-08-09 21:21:50 +0100

    The majority of the win/lose/draw game logic has been written.

--- Noughts and Crosses/scripts/game.js
+++ Noughts and Crosses/scripts/game.js
@@ -1,13 +1,15 @@
 $(function() 
 {
-    let playerModels = {
+    let playerModels = 
+    {
         NOUGHT: 0,
         CROSS: 1
     };
 
-    let playerModel = playerModels.NOUGHT;
+    let currentPlayer = playerModels.NOUGHT;
 
-    let winningCombinations = [
+    let winningCombinations = 
+    [
         //Horizontal
         [0, 1, 2],
         [3, 4, 5],
@@ -21,14 +23,70 @@ $(function()
         [2, 4, 6]
     ]
     
-    let currentBoardCombination = [];
+    let gameBoard = [ "E", "E", "E",
+                      "E", "E", "E",
+                      "E", "E", "E" ];
+
+    let currentTurnCount = 0;
 
     $("#game-mat > div").click(function() 
     {
-        if($.inArray($(this).index(), currentBoardCombination) === -1)
+        if(currentPlayer == playerModels.NOUGHT)
         {
-            (playerModel === playerModels.NOUGHT) ? $(this).addClass("nought").text("O") : $(this).addClass("cross").text("X");
-            currentBoardCombination.push($(this).index());
+            $(this).addClass("nought").text("O");
+            gameBoard[$(this).index()] = "O";
         }
+        else
+        {
+             $(this).addClass("cross").text("X");
+            gameBoard[$(this).index()] = "X";
+        }
+        
+        currentPlayer = !currentPlayer;
+        currentTurnCount ++;
+        GetWinner();
     });
+
+    function GetWinner()
+    {
+        if(DoesPlayerWin("O"))
+        {
+            alert("Nought wins.");
+        }
+        else if(DoesPlayerWin("X"))
+        {
+            alert("Cross wins.");
+        }
+        else if (currentTurnCount == 9)
+        {
+            alert("It's a draw.");
+        }
+    }
+
+    function IsADraw()
+    {
+        return (gameBoard.indexOf("E") === -1);
+    }
+
+    function DoesPlayerWin(playerChar)
+    {
+        let playerWins = false;
+        for(let i = 0; i < winningCombinations.length; i++)
+        {
+            let foundValues = 0;
+
+            for(let j = 0; j < 3; j++)
+            {
+                foundValues += (gameBoard[winningCombinations[i][j]] == playerChar)
+            }
+
+            if(foundValues == 3)
+            {
+                playerWins = true;
+                break;
+            }
+        }
+
+        return playerWins;
+    }
 });
